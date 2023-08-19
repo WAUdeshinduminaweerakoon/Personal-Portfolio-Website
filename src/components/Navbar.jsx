118c5e71896942f82a4156fe38adcf4ef88ab1ac
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-08-20 00:23:24 +0530

    add the css in body and text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="nav">
+        <div className="nav__menu">
+            <ul className="nav__list"></ul>
+
+        </div>
+    </nav>
   )
 }
 
