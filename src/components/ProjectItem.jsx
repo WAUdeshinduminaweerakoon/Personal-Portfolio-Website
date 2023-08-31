34f7cd1b709b4a1a050e1eecf1f3ba14e061002b
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-08-31 16:27:30 +0530

    add deteles in project first

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Clase from "../assets/close.svg";
 
-const ProjectItem = ({img, title, detaile}) => {
+const ProjectItem = ({img, title, details}) => {
   return (
     <div className='project__item'>
-        <img src={img} alt="" className="project__img" />
+      <img src={img} alt="" className="project__img" />
+      <div className='project__hover'>
+        <h3 className="project__title">{title}</h3>
+
+      </div>
+      <div className='project__modal'>
+        <div className="project__modal-content">  
+          <img src={Clase} alt="" className="modal__close" />
+          <h3 className='modal__title'>{title}</h3>
+          <ul className="modal__list grid">
+             {details.map(({icon, title,desc}, index)=>{
+              return(
+                <li className="modal__item" key={index}>
+                  <span className="item__icon">{icon}</span>
+                </li>
+
+              )
+             })}
+          </ul>
+        </div>
+
+      </div>
 
     </div>
   )
